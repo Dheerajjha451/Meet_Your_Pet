a30fc0c669462923b3e18e94273b449410446042
Dheeraj Jha <106474979+Dheerajjha451@users.noreply.github.com>
2024-03-24 18:12:02 +0530

    Update Upload.jsx

--- frontend/src/assets/Components/Upload.jsx
+++ frontend/src/assets/Components/Upload.jsx
@@ -57,7 +57,7 @@ function UploadImage() {
         {!file ? (
           <label
             htmlFor="image-upload"
-            className="cursor-pointer bg-indigo-500 text-white px-4 py-2 rounded flex items-center justify-center"
+            className="cursor-pointer bg-customBlue text-white px-4 py-2 rounded flex items-center justify-center"
           >
             <FontAwesomeIcon icon={faUpload} className="mr-4" />
             Upload Image
