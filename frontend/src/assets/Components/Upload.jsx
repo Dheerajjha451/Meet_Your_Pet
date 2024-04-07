a7a21a73735a3ff394d5ecb3d7dd37a0618a03eb
Dheeraj Jha <dheerajjha451@gmail.com>
2024-04-07 11:38:54 +0530

    url

--- frontend/src/assets/Components/Upload.jsx
+++ frontend/src/assets/Components/Upload.jsx
@@ -28,7 +28,7 @@ function UploadImage() {
       const formData = new FormData();
       formData.append("file", file);
 
-      const response = await fetch("http://localhost:5000/predict", {
+      const response = await fetch("https://meet-your-pet-whok.vercel.app/predict", {
         method: "POST",
         body: formData,
       });
