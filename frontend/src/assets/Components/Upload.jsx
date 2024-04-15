ef216a2bc010a442e58c42d61d245fd2554cd06c
Dheeraj Jha <106474979+Dheerajjha451@users.noreply.github.com>
2024-04-15 15:54:32 +0530

    Update Upload.jsx

--- frontend/src/assets/Components/Upload.jsx
+++ frontend/src/assets/Components/Upload.jsx
@@ -27,10 +27,10 @@ function UploadImage() {
     try {
       const formData = new FormData();
       formData.append("file", file);
-      const response = await fetch("https://backendmyp.onrender.com/predict", {
+      const response = await fetch("http://127.0.0.1:5000/predict", {
         method: "POST",
         body: formData,
-      });    
+      });
 
       if (!response.ok) {
         console.error("Server response was not ok", response);
